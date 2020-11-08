eacc4dc2ee26b9922fd3ff2d62227333c13a5a83
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-11-08 15:20:32 +0100

    LinearLayout added

--- app/src/main/java/com/com/layoutexample/LinearLayout.kt
+++ app/src/main/java/com/com/layoutexample/LinearLayout.kt
@@ -2,15 +2,10 @@ package com.com.layoutexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class LinearLayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        linearLayoutButton.setOnClickListener{
-
-        }
+        setContentView(R.layout.activity_linear_layout)
     }
 }
